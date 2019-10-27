0523daf34e68da48c27398893997d1c5afb96c3d
Christopher <christopher@bilderlahe.info>
2019-10-27 18:12:43 +0100

    added executability from command prompt

--- ImageSorter.py
+++ ImageSorter.py
@@ -3,23 +3,24 @@ move or copy images in directories named by the dates the pictures were taken
 
 Christopher Mertens
 2019-10-20
-version: 0.1.2
+version: 0.2.2
 """
 
 import glob
 import os
 import shutil
 import sys
+import argparse
 from PIL import Image
 
 
-def sort_by_date_taken(src_dir, dst_dir=None, file_operation='cp', file_name_extensions=['jpg'], **kwargs):
+def sort_by_date_taken(src_dir, dst_dir=None, file_operation='cp', filename_extensions=['jpg'], **kwargs):
     """
     copy images into directories named by the date when the picture was taken
     :param src_dir: directory to copy the files from
     :param dst_dir: target directory to copy the files to
     :param file_operation: file operation to perform on the files
-    :param file_name_extensions: filter the files by extension
+    :param filename_extensions: filter the files by extension
     """
 
     def _get_date_taken(path):
@@ -52,11 +53,11 @@ def sort_by_date_taken(src_dir, dst_dir=None, file_operation='cp', file_name_ext
         dst_dir = src_dir
     # find all files with specified file name extension
     files = []
-    for file_name_extension in file_name_extensions:
+    for filename_extension in filename_extensions:
         if 'read_recursive' in kwargs.keys() and kwargs['read_recursive']:
-            files += glob.glob(src_dir + "\\**\\*." + file_name_extension, recursive=True)
+            files += glob.glob(src_dir + "\\**\\*." + filename_extension, recursive=True)
         else:
-            files += glob.glob(src_dir + "\\*." + file_name_extension)
+            files += glob.glob(src_dir + "\\*." + filename_extension)
     print("copying " + str(len(files)) + " files from " + src_dir + " to " + dst_dir + '\n')
     for num, file in enumerate(files):
         # create the name of directory structure
@@ -84,6 +85,37 @@ def sort_by_date_taken(src_dir, dst_dir=None, file_operation='cp', file_name_ext
 
 
 if __name__ == '__main__':
+
+    # parse command prompt arguments
+    parser = argparse.ArgumentParser(description="sort images by date taken")
+
+    parser.add_argument('--file_operation', action='store', default='cp')
+    parser.add_argument('--filename_extensions', '-ext', action='store', nargs='*', default=['jpg'])
+    parser.add_argument('--dir_structure', '-struc', action='store', default='ymd')
+    parser.add_argument('--read_recursive', '-rr', action='store_true')
+    parser.add_argument('src_dir', action='store')
+    parser.add_argument('--dst_dir', action='store', default=None)
+
+    args = parser.parse_args()
+
+    # check arguments validity
+    if args.file_operation not in ['cp', 'mv', 'copy', 'move']:
+        parser.error('invalid file_operation:' + args.file_operation)
+    if not any(char in args.dir_structure for char in ['y', 'm', 'd']):
+        parser.error('invalid option for dir_structure: ' + args.dir_structure)
+
+    # get absolute path
+    src_dir = os.path.abspath(args.src_dir)
+    if not os.path.exists(src_dir):
+        parser.error("source directory doesn't exist")
+    dst_dir = os.path.abspath(args.dst_dir) if args.dst_dir is not None else None
+
+    sort_by_date_taken(src_dir=src_dir, dst_dir=dst_dir, file_operation=args.file_operation,
+                       filename_extensions=args.filename_extensions,
+                       dir_structure=args.dir_structure, read_recursive=args.read_recursive)
+
+    """ uncomment for executing without command prompt
     # enter the src dir to sort
     src_dir = os.path.abspath("test_images\\test_sorted")
     sort_by_date_taken(src_dir, file_operation='mv', dir_structure='ymd', read_recursive=True)
+    """
